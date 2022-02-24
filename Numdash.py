b8323faca6e29c4cb2974cc525258b884bec0aba
stefimaz <89653430+stefimaz@users.noreply.github.com>
2022-02-24 10:52:49 -0500

    updates

--- Numdash.py
+++ Numdash.py
@@ -6,6 +6,8 @@ from dotenv import load_dotenv
 import streamlit as st
 from PIL import Image
 
+from pinata import pin_file_to_ipfs, pin_json_to_ipfs, convert_data_to_json
+
 st.set_page_config(page_title='Numisma: Diversify your crypto holdings', layout='wide')
 st.image("./Images/Cryptos.jpeg")
 st.title("Numisma. Crypto Index Portfolio Management")
@@ -46,14 +48,14 @@ w3 = Web3(Web3.HTTPProvider(os.getenv("WEB3_PROVIDER_URI")))
 # Connects to the contract using the contract address and ABI
 # loading contract fot --------- token index
 @st.cache(allow_output_mutation=True)
-def load_contract():
+def load_contract1():
 
     # Load the contract ABI
-    with open(Path('./Ventidex3_abi.json')) as f:
+    with open(Path('./Ventidex2_abi.json')) as f:
         contract_abi = json.load(f)
 
     # Set the contract address (this is the address of the deployed contract)
-    contract_address = os.getenv("SMART_CONTRACT_ADDRESS3")
+    contract_address = os.getenv("SMART_CONTRACT_ADDRESS2")
 
     # Get the contract
     contract = w3.eth.contract(
@@ -63,7 +65,7 @@ def load_contract():
     return contract
 
 # Load the contract
-contract = load_contract()
+contract1 = load_contract1()
 
 # The contracts have to be loaded separately for eack Token index
 # loading contract fot --------- token index
@@ -124,17 +126,71 @@ accounts = w3.eth.accounts
 
 # Use a streamlit component to get the address of the artwork owner from the user
 address = st.selectbox("Select your wallet", accounts)
-amount = st.number_input("How many shares do you want to buy?")
+amount = st.number_input("How many shares do you want to buy?", min_value=1, value=1, step=1)
 # Use a streamlit component to get the contract URI
 # contract_uri = st.text_input("The URI to the artwork")
 
 if st.button("Buy Now"):
 
     # Use the contract to send a transaction to the safeMint function
-    tx_hash = contract.functions.safeMint(address, amount).transact({
-        "from": address, "gas": 1000000})
+    tx_hash = contract3.functions.safeMint(address, amount).transact({
+        "from": address, "gas": 3200000})
 
     receipt = w3.eth.waitForTransactionReceipt(tx_hash)
     st.write("Transaction receipt mined:")
     st.success(dict(receipt))
 st.markdown("---")
+
+################################################################################
+# Helper functions to pin files and json to Pinata
+################################################################################
+
+
+def pin_index(index_name, index_file):
+    # Pin the file to IPFS with Pinata
+    ipfs_file_hash = pin_file_to_ipfs(index_file.getvalue())
+
+    # Build a token metadata file for the artwork
+    token_json = {
+        "name": index_name,
+        "image": ipfs_file_hash
+    }
+    json_data = convert_data_to_json(token_json)
+
+    # Pin the json to IPFS with Pinata
+    json_ipfs_hash = pin_json_to_ipfs(json_data)
+
+    return json_ipfs_hash
+
+
+def pin_index_report(report_content):
+    json_report = convert_data_to_json(report_content)
+    report_ipfs_hash = pin_json_to_ipfs(json_report)
+    return report_ipfs_hash
+
+
+################################################################################
+# Register New Artwork
+################################################################################
+st.markdown("## Register Your Portfolio")
+index_name = st.text_input("Enter the name of the artwork")
+holder_name = st.text_input("Enter your name")
+initial_index_value = st.text_input("Enter the initial index amount")
+file = st.file_uploader("Upload Artwork", type=["jpg", "jpeg", "png"])
+if st.button("Register Portfolio"):
+    # Use the `pin_artwork` helper function to pin the file to IPFS
+    index_ipfs_hash =  pin_index(index_name, file)
+    index_uri = f"ipfs://{index_ipfs_hash}"
+    tx_hash = contract3.functions.registerPortfolio(
+        address,
+        index_name,
+        holder_name,
+        int(initial_index_value)
+    ).transact({'from': address, 'gas': 1000000})
+    receipt = w3.eth.waitForTransactionReceipt(tx_hash)
+    st.write("Transaction receipt mined:")
+    st.write(dict(receipt))
+    st.write("You can view the pinned metadata file with the following IPFS Gateway Link")
+    st.markdown(f"[Artwork IPFS Gateway Link](https://ipfs.io/ipfs/{index_ipfs_hash})")
+st.markdown("---")
+
