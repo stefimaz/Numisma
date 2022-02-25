b0e466fa711c5b1a535bb4c5797e7aa8f281326b
stefimaz <89653430+stefimaz@users.noreply.github.com>
2022-02-25 17:56:19 -0500

    Trying metamask connection

--- .ipynb_checkpoints/Numdash-checkpoint.py
+++ .ipynb_checkpoints/Numdash-checkpoint.py
@@ -194,3 +194,59 @@ if st.button("Register Portfolio"):
     st.markdown(f"[Artwork IPFS Gateway Link](https://ipfs.io/ipfs/{index_ipfs_hash})")
 st.markdown("---")
 
+#############################################################
+
+# import useState, useEffect from react
+# import Calendar from './components/Calendar';
+import detectEthereumProvider from '@metamask/detect-provider';
+
+function App() {
+  const [account, setAccount] = useState(false);
+
+  useEffect(() => {
+    isConnected();
+  }, []);
+
+  const isConnected = async () => {
+    const provider = await detectEthereumProvider();
+    const accounts = await provider.request({ method: "eth_accounts" });
+
+    if (accounts.length > 0) {
+      setAccount(accounts[0]);
+    } else {
+      console.log("No authorized account found")
+    }
+  }
+
+  const connect = async () => {
+    try {
+      const provider = await detectEthereumProvider();
+
+      // returns an array of accounts
+      const accounts = await provider.request({ method: "eth_requestAccounts" });
+
+      // check if array at least one element. if so, set account to the first one.
+      if (accounts.length > 0) {
+        console.log('account found', accounts);
+        setAccount(accounts[0]);
+      } else {
+        alert('No account found');
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>calend3</h1>
+        <div id="slogan">web3 appointment scheduling</div>
+        {!account && <button onClick={connect}>connect wallet</button>}
+        {account && <Calendar />}
+      </header>
+    </div>
+  );
+}
+
+export default App;
