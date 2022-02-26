6d609b04343eac5b2aedb569eda7580f250b637f
Briggs Lalor <90216031+briggslalor@users.noreply.github.com>
2022-02-26 11:33:59 -0500

    get_coin_values update
    Updated to distribute investment amount across the different coins and determine how many of each coin is owned

--- get_index_data.py
+++ get_index_data.py
@@ -1,6 +1,43 @@
 import CryptoDownloadData as coinData
 import pandas as pd
 
+from pycoingecko import CoinGeckoAPI
+cg = CoinGeckoAPI()
+
+coinbase = {'BTC' : {'CoinGeckID' : 'bitcoin'},
+            'ETH' : {'CoinGeckID' : 'ethereum'}, 
+            'BNB' : {'CoinGeckID' : 'binancecoin'}, 
+            'USDC' : {'CoinGeckID' : 'usd-coin'}, 
+            'XRP' : {'CoinGeckID' : 'ripple'}, 
+            'ADA' : {'CoinGeckID' : 'cardano'}, 
+            'SOL' : {'CoinGeckID' : 'solana'}, 
+            'LUNA' : {'CoinGeckID' : 'terra-luna'}, 
+            'AVAX' : {'CoinGeckID' : 'avalanche-2'}}
+
+metaverse = {'MANA' : {'CoinGeckID' : 'decentraland'}, 
+             'SAND' : {'CoinGeckID' : 'the-sandbox'}, 
+             'AXS' : {'CoinGeckID' : 'axie-infinity'}, 
+             'THETA' : {'CoinGeckID' : 'theta-token'}, 
+             'ENJ' : {'CoinGeckID' : 'enjincoin'},
+             'WEMIX' : {'CoinGeckID' : 'wemix-token'}, 
+             'WAXP' : {'CoinGeckID' : 'wax'}, 
+             'RNDR' : {'CoinGeckID' : 'render-token'}, 
+             'SUSHI' : {'CoinGeckID' : 'sushi'}, 
+             'ONT' : {'CoinGeckID' : 'ontology'}, 
+             'UOS' : {'CoinGeckID' : 'ultra'}, 
+             'PLA' : {'CoinGeckID' : 'playdapp'}, 
+             'CEEK' : {'CoinGeckID' : 'ceek'}, 
+             'CHR' : {'CoinGeckID' : 'chromaway'}}
+
+yieldfarm = {'CAKE' : {'CoinGeckID' : 'pancakeswap-token'}, 
+             'AAVE' : {'CoinGeckID' : 'aave'}, 
+             'CRV' : {'CoinGeckID' : 'curve-dao-token'}, 
+             'RLY' : {'CoinGeckID' : 'rally-2'}, 
+             'SNX' : {'CoinGeckID' : 'havven'}, 
+             'SUSHI' : {'CoinGeckID' : 'sushi'}, 
+             'RGT' : {'CoinGeckID' : 'rari-governance-token'}, 
+             'REEF' : {'CoinGeckID' : 'reef-finance'}}
+
 def get_index_prices(selected_index):
     if selected_index == 'Coinbase100':
         index_coins = ['BTC', 'ETH', 'BNB', 'USDC', 'XRP', 'ADA', 'SOL', 'LUNA', 'AVAX']
@@ -36,3 +73,35 @@ def get_index_coins(selected_index):
 def get_index_df(coins, weights):
     index_df = pd.DataFrame({'Coins' : coins, 'Weights' : weights})
     return index_df
+
+
+def get_coin_values(investment, selected_index, index_df):
+    
+    if selected_index == 'Coinbase100':
+        index_dict = coinbase
+        index_coins = ['BTC', 'ETH', 'BNB', 'USDC', 'XRP', 'ADA', 'SOL', 'LUNA', 'AVAX']
+    elif selected_index == 'TopMetaverseTokens':
+        index_dict = metaverse
+        index_coins = ['MANA', 'SAND', 'AXS', 'THETA', 'ENJ','WEMIX', 'WAXP', 'RNDR', 'SUSHI', 'ONT', 'UOS', 'PLA', 'CEEK', 'CHR']
+    elif selected_index == 'YieldFarmingTokens':
+        index_dict = yieldfarm
+        index_coins = ['CAKE', 'AAVE', 'CRV', 'RLY', 'SNX', 'SUSHI', 'RGT', 'REEF']
+    else:
+        return ('ERROR: Invalid Index Entry.')
+    
+    index_df['Investment per Coin (USD)'] = investment * index_df['Weights']
+    
+    coin_prices = []
+    
+    for i in index_coins:
+        coin = i
+        cg_id = index_dict[coin]['CoinGeckID']
+        price = cg.get_price(ids = cg_id, vs_currencies = 'usd')
+        coin_prices.append(price[cg_id]['usd'])
+        
+    index_df['Coin Price (USD)'] = coin_prices
+    
+    index_df['Coins Owned'] = index_df['Investment per Coin (USD)'] / index_df['Coin Price (USD)']
+    
+    return index_df
+    
