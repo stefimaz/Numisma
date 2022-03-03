4582e3d510d0f3cb6d88a7f8edd55997cb40b28f
Ken Lee <90640710+klee08@users.noreply.github.com>
2022-03-02 23:41:34 -0500

    NumismaMain

--- get_index_data.py
+++ get_index_data.py
@@ -38,6 +38,42 @@ yieldfarm = {'CAKE' : {'CoinGeckID' : 'pancakeswap-token'},
              'RGT' : {'CoinGeckID' : 'rari-governance-token'}, 
              'REEF' : {'CoinGeckID' : 'reef-finance'}}
 
+
+allindex = {'CAKE' : {'CoinGeckID' : 'pancakeswap-token'}, 
+            'AAVE' : {'CoinGeckID' : 'aave'}, 
+            'CRV' : {'CoinGeckID' : 'curve-dao-token'}, 
+            'RLY' : {'CoinGeckID' : 'rally-2'}, 
+            'SNX' : {'CoinGeckID' : 'havven'}, 
+            'SUSHI' : {'CoinGeckID' : 'sushi'}, 
+            'RGT' : {'CoinGeckID' : 'rari-governance-token'}, 
+            'REEF' : {'CoinGeckID' : 'reef-finance'},
+            'BTC' : {'CoinGeckID' : 'bitcoin'},
+            'ETH' : {'CoinGeckID' : 'ethereum'}, 
+            'BNB' : {'CoinGeckID' : 'binancecoin'}, 
+            'USDC' : {'CoinGeckID' : 'usd-coin'}, 
+            'XRP' : {'CoinGeckID' : 'ripple'}, 
+            'ADA' : {'CoinGeckID' : 'cardano'}, 
+            'SOL' : {'CoinGeckID' : 'solana'}, 
+            'LUNA' : {'CoinGeckID' : 'terra-luna'}, 
+            'AVAX' : {'CoinGeckID' : 'avalanche-2'},
+            'MANA' : {'CoinGeckID' : 'decentraland'}, 
+            'SAND' : {'CoinGeckID' : 'the-sandbox'}, 
+            'AXS' : {'CoinGeckID' : 'axie-infinity'}, 
+            'THETA' : {'CoinGeckID' : 'theta-token'}, 
+            'ENJ' : {'CoinGeckID' : 'enjincoin'},
+            'WEMIX' : {'CoinGeckID' : 'wemix-token'}, 
+            'WAXP' : {'CoinGeckID' : 'wax'}, 
+            'RNDR' : {'CoinGeckID' : 'render-token'}, 
+            'SUSHI' : {'CoinGeckID' : 'sushi'}, 
+            'ONT' : {'CoinGeckID' : 'ontology'}, 
+            'UOS' : {'CoinGeckID' : 'ultra'}, 
+            'PLA' : {'CoinGeckID' : 'playdapp'}, 
+            'CEEK' : {'CoinGeckID' : 'ceek'}, 
+            'CHR' : {'CoinGeckID' : 'chromaway'}}
+
+
+
+
 def get_index_prices(selected_index):
     if selected_index == 'Coinbase100':
         index_coins = ['BTC', 'ETH', 'BNB', 'USDC', 'XRP', 'ADA', 'SOL', 'LUNA', 'AVAX']
@@ -104,4 +140,23 @@ def get_coin_values(investment, selected_index, index_df):
     index_df['Coins Owned'] = index_df['Investment per Coin (USD)'] / index_df['Coin Price (USD)']
     
     return index_df
+
+
+def get_coin_values_by_weight_df(investment, weight_df):
     
+    index_dict = allindex
+    weight_df['weight'] = pd.to_numeric(weight_df['weight'])
+    weight_df['investment'] = investment * weight_df['weight']
+    
+    coin_prices = []
+    
+    for symbol in weight_df['symbol']:
+        cg_id = index_dict[symbol]['CoinGeckID']
+        price = cg.get_price(ids = cg_id, vs_currencies = 'usd')
+        coin_prices.append(price[cg_id]['usd'])
+        
+    weight_df['coin_px'] = coin_prices
+    
+    weight_df['coin_cnt'] = weight_df['investment'] / weight_df['coin_px']
+    
+    return weight_df
